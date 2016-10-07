f698c37f1d54225305b19068c322348e2a7e23c2
RMLOY_000 <RMLOY_000@Rodrigo>
2016-10-06 18:25:31 -0600

    10/6/2016 - initial commit to repository

--- CIT260-Game-of-Life---Team-B/src/cit260/game/of/life/team/b/CIT260GameOfLifeTeamB.java
+++ CIT260-Game-of-Life---Team-B/src/cit260/game/of/life/team/b/CIT260GameOfLifeTeamB.java
@@ -5,6 +5,8 @@
  */
 package cit260.game.of.life.team.b;
 
+import byui.cit260.gameOfLife.model.Choice;
+import byui.cit260.gameOfLife.model.Item;
 import byui.cit260.gameOfLife.model.Player;
 
 /**
@@ -18,13 +20,35 @@ public class CIT260GameOfLifeTeamB {
      */
     public static void main(String[] args) {
         Player playerOne = new Player();
-        
+        Item  itemOne = new Item();
+        Choice choiceOption =  new Choice();
+                
+         //player class       
         playerOne.setName("Rodrigo");
         playerOne.setBestScore(20);
         playerOne.setAttribute("Faith");
-                
+        
+        
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+        //item class
+        itemOne.setInventoryType("Scripture");
+        itemOne.setQuantityInStock(4);
+        itemOne.setRequiredAmount(4);
+        
+        String itemType = itemOne.toString();
+        System.out.println(itemType);
+        
+        //choice class
+        
+        choiceOption.setChoiceList("b,c,d");
+        choiceOption.setChoicePoints(2);
+        
+        String choiceResult = choiceOption.toString();
+        System.out.println(choiceResult);
+       
+        
         
     }
     

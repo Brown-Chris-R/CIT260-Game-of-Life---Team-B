af77074a33b122eabbc33502d4fab3544001b8be
ferdycip <ferdycip@gmail.com>
2016-10-06 22:17:12 -0600

    10/6 class creation

---
+++ CIT260-Game-of-Life---Team-B/src/byui/cit260/gameOfLife/model/Scene.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.gameOfLife.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ferdycipriano
+ */
+public class Scene implements Serializable{
+    private String description;
+    private String blockedLocations;
+
+    public Scene() {
+    }
+    
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBlockedLocations() {
+        return blockedLocations;
+    }
+
+    public void setBlockedLocations(String blockedLocations) {
+        this.blockedLocations = blockedLocations;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.description);
+        hash = 97 * hash + Objects.hashCode(this.blockedLocations);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.blockedLocations, other.blockedLocations)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Scene{" + "description=" + description + ", blockedLocations=" + blockedLocations + '}';
+    }
+    
+    
+    
+    
+}

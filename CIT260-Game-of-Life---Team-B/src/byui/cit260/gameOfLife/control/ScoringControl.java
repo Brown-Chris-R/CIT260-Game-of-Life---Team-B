8dd3dc68d1bfdccbdd859d2d9a47c81a4953f1f4
cbrown <cbrown@CFO-FWH9J12.plumcreek.net>
2016-10-20 07:25:52 -0600

    Coded the calcOperation function in the ScoringControl class.

--- CIT260-Game-of-Life---Team-B/src/byui/cit260/gameOfLife/control/ScoringControl.java
+++ CIT260-Game-of-Life---Team-B/src/byui/cit260/gameOfLife/control/ScoringControl.java
@@ -10,5 +10,33 @@ package byui.cit260.gameOfLife.control;
  * @author cbrown
  */
 public class ScoringControl {
+    String errorText = new String();
     
+public float calcOperation(float hospitalBillAmt, float insuranceDeductibleAmt, float insuranceCoverageAmt ) {
+    float amountOwed;
+    
+    if (hospitalBillAmt <= 0) {
+        errorText = "Hospital Bill Amount is <= Zero";
+        return -1;
+    } else if (hospitalBillAmt > 30000) {
+        errorText = "Hospital Bill Amount is > 30,000";
+        return -1;
+    } else if (insuranceDeductibleAmt < 0) {      
+        errorText = "Insurance Deductible Amount is < 0";
+        return -1;
+    } else if (insuranceDeductibleAmt > 500) {      
+        errorText = "Insurance Deductible Amount is > 500";
+        return -1;
+    } else if (insuranceCoverageAmt < 0) {      
+        errorText = "Insurance Coverage Amount is < 0";
+        return -1;
+    } else if (insuranceCoverageAmt > 100) {      
+        errorText = "Insurance Coverage Amount is > 100%";
+        return -1;
+    }
+    
+    amountOwed = (hospitalBillAmt - insuranceDeductibleAmt) * (1-insuranceCoverageAmt) + insuranceDeductibleAmt;
+    return amountOwed;
+}
+
 }

db94fd340066b2a24eba103ab742708214d28a1c
cbrown <cbrown@CFO-FWH9J12.plumcreek.net>
2016-10-29 12:34:17 -0600

    Created the StartProgramView class and functions.

---
+++ CIT260-Game-of-Life---Team-B/src/byui/cit260/gameOfLife/view/MainMenuView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.gameOfLife.view;
+
+/**
+ *
+ * @author cbrown
+ */
+class MainMenuView {
+
+    void displayMainMenuView() {
+        System.out.println("\n*** displayMainMenuView() function called ***");
+    }
+    
+}
